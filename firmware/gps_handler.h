fccacaf5459a3109e54344e3f20f4f0a6ad9ae8d
askakasdhhe <107674428+askakasdhhe@users.noreply.github.com>
2026-08-03 09:22:50 +0800

    Delete firmware directory

--- firmware/gps_handler.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-void gps_init();
-void gps_send_if_due(unsigned long now, bool connected);
--- firmware/onenet_handler.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-void onenet_init();
-void onenet_upload_gps(double lat, double lng, double speed);
-bool onenet_is_connected();
